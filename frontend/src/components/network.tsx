d68769426111b90042c510a9ca550272aeee849f
Sharleen Jamoza <sharleenjamoza@gmail.com>
2025-02-25 22:21:39 +0300

    updated network data

--- frontend/src/components/network.tsx
+++ frontend/src/components/network.tsx
@@ -1,12 +1,28 @@
 import networks from "./networkData";
 function DisplayNetworks(){
+    console.log(networks);
+    const handleSelect = (networkId: any) => {
+        localStorage.setItem('networkId', networkId);
+    }
+    console.log(handleSelect(networks.id));
+    
     return(
         <>
             <div>
                 <h1>Select a Network</h1>
-                <select name="Networks" id="network">
-                
-                </select>
+                <div>
+                    {networks.map((network: any) => (
+                        <div key={network.id}
+                        onClick={() => handleSelect(networks.id)}
+                        >
+                            {network.name}
+                            <img src={network.imag} alt={network.name} />
+                            <p>{network.name}</p>
+                        </div>
+
+                    ) )
+                    }
+                </div>
             </div>
 
         </>
--- frontend/src/components/networkData.ts
+++ frontend/src/components/networkData.ts
@@ -1,3 +1,10 @@
+// interface Network {
+//     id: string;
+//     name: string;
+//     image: string;
+//     rpc: string;
+// };
+
 const networks : any = [
     {
         id: "Ethereum",
@@ -8,50 +15,50 @@ const networks : any = [
     {
         id: "OP Mainnet",
         name: " Optimism Mainnet",
-        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
         rpc: "https://opt-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
     },
     {
         id: "Arbitrum",
         name: "Arbitrum Mainnet",
-        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
         rpc: "https://arb-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
     },
     {
         id: "Polygon",
         name: "Polygon Mainnet",
-        //image: "https,
+        image: "https",
         rpc: "https://polygon-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
         
     },
     {
         id: "Fantom Opera",
         name: "Fantom Mainnet",
-        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
         rpc: "https://fantom-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
     },
     {
         id: "Avalanche",
         name: "Avalanche Mainnet",
-        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
         rpc: "https://avalanche-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
     },
     {
         id : "Base",
         name: "Base Mainnet",
-        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
         rpc: "https://base-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
     },
     {
         id: "Linea",
         name: "Linea Mainnet",
-        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
         rpc: "https://linea-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
     },
     {
         id: "Unichain",
         name: "Unichain Mainnet",
-        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
         rpc: "https://unichain-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
     }   
 ]
