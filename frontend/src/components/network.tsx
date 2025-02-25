c523077732f01457f2ea10f6987fe6507744c21e
Sharleen Jamoza <sharleenjamoza@gmail.com>
2025-02-25 12:23:05 +0300

    added a networks file

---
+++ frontend/src/components/network.tsx
@@ -0,0 +1,15 @@
+import networks from "./networkData";
+function DisplayNetworks(){
+    return(
+        <>
+            <div>
+                <h1>Select a Network</h1>
+                <select name="Networks" id="network">
+                
+                </select>
+            </div>
+
+        </>
+    )
+}
+export default DisplayNetworks;
---
+++ frontend/src/components/networkData.ts
@@ -0,0 +1,59 @@
+const networks : any = [
+    {
+        id: "Ethereum",
+        name: "Ethereum Mainnet",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://eth-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    },
+    {
+        id: "OP Mainnet",
+        name: " Optimism Mainnet",
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://opt-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    },
+    {
+        id: "Arbitrum",
+        name: "Arbitrum Mainnet",
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://arb-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    },
+    {
+        id: "Polygon",
+        name: "Polygon Mainnet",
+        //image: "https,
+        rpc: "https://polygon-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+        
+    },
+    {
+        id: "Fantom Opera",
+        name: "Fantom Mainnet",
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://fantom-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    },
+    {
+        id: "Avalanche",
+        name: "Avalanche Mainnet",
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://avalanche-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    },
+    {
+        id : "Base",
+        name: "Base Mainnet",
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://base-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    },
+    {
+        id: "Linea",
+        name: "Linea Mainnet",
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://linea-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    },
+    {
+        id: "Unichain",
+        name: "Unichain Mainnet",
+        //image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Ethereum-icon-purple.svg/1200px-Ethereum-icon-purple.svg.png",
+        rpc: "https://unichain-mainnet.g.alchemy.com/v2/t0q4rmOWqfNSwebEsVtHyqYzVK3mFZSU"
+    }   
+]
+
+export default networks;
